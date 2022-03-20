e429ea44b63abd9a903f16be6399611335642c56
Nicholas Miazzo <nicholas.miazzo1998@gmail.com>
2022-03-20 22:46:50 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from typing import Dict, List, TextIO
+from typing import Dict, List, TextIO, Optional
 
 
 @dataclass
@@ -14,8 +14,26 @@ class AdjacencyNode:
 
 
 @dataclass
+class Edge:
+    a: int
+    b: int
+    weight: int
+
+    def __init__(self, a: int, b: int, weight: int) -> None:
+        self.a = a
+        self.b = b
+        self.weight = weight
+
+
+@dataclass
 class Graph:
-    nodes: Dict[int, List[AdjacencyNode]]
+    adjacency_list: Dict[int, List[AdjacencyNode]]
+
+    # TODO campo ridondante
+    # valutare se abbia senso mantenere una lista di archi nonostante si possa ottenere dinamicamente
+    # inoltre non credo che mantenere questo campo sia corretto per l'implementazione "liste di adiacenza"
+    edges: List[Edge]
+
     n: int
     m: int
 
@@ -23,22 +41,55 @@ class Graph:
         self.n = n
         self.m = m
 
-        self.nodes = {}
-        for i in range(1, n + 1):
-            self.nodes[i] = []
+        self.adjacency_list = {}
+        self.edges = []
 
     def add_edge(self, a: int, b: int, weight: int) -> None:
-        self.nodes[a].append(AdjacencyNode(b, weight))
-        self.nodes[b].append(AdjacencyNode(a, weight))
+        if a not in self.adjacency_list:
+            self.adjacency_list[a] = []
+        if b not in self.adjacency_list:
+            self.adjacency_list[b] = []
+
+        # no self loops
+        if a != b:
+            self.adjacency_list[a].append(AdjacencyNode(b, weight))
+            self.adjacency_list[b].append(AdjacencyNode(a, weight))
+
+            if self.get_edge(a, b) is None:
+                self.edges.append(Edge(a, b, weight))
+
+    def get_all_edges(self) -> List[Edge]:
+        return self.edges
+
+    def get_edge(self, a: int, b: int) -> Optional[Edge]:
+        for adj_node in self.adjacency_list[a]:
+            if adj_node.name == b:
+                return Edge(a, b, adj_node.weight)
+        return None
+
+    # get all arcs of given node
+    def get_node_edges(self, node_name: int) -> Optional[List[AdjacencyNode]]:
+        if node_name not in self.adjacency_list:
+            return None
+        return self.adjacency_list[node_name]
+
+    def get_all_nodes(self) -> Dict[int, List[AdjacencyNode]]:
+        return self.adjacency_list
+
+    def get_weight(self, a: int, b: int) -> int:
+        return self.get_edge(a, b).weight
 
 
 def graph_from_file(filename: str) -> Graph:
     file: TextIO = open("dataset/" + filename, "r")
-    n: int = int(file.readline().split()[0])
+    first_line_data: List[str] = file.readline().split()
+    n: int = int(first_line_data[0])
+    m: int = int(first_line_data[1])
 
-    graph: Graph = Graph(n)
+    graph: Graph = Graph(n, m)
     for line in file.readlines():
         data: List[str] = line.split()
         graph.add_edge(int(data[0]), int(data[1]), int(data[2]))
 
+    file.close()
     return graph
