4941414a7d9ef20be88f488a9c28cf76ed5ec2ad
Nicholas Miazzo <nicholas.miazzo1998@gmail.com>
2022-03-22 17:25:25 +0100

    FIx Content constructor mutable argument

--- 01-MST/parser.py
+++ 01-MST/parser.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 from typing import Dict, List, TextIO, Tuple
 
+
 @dataclass
 class Content:
     """
-    A class for rappresent the content of a file after it is parsed.
+    A class for represent the content of a file after it is parsed.
     This class is designed for file produced by the stanford-algs' TestCaseGenerator.
     ...
 
@@ -15,25 +16,30 @@ class Content:
     m: int
         number of edges inside the graph described in the file
     list_triple: List[Tuple[int, ...]]
-        list of triple rappresented the edge that has as origin node as first element,
+        list of triple represented the edge that has as origin node as first element,
         target node as second element and weight the third element.
     """
     n: int
     m: int
     list_triple: List[Tuple[int, ...]]
 
-    def __init__(self, n: int = 0, m: int = 0, list_triple: List[Tuple[int, ...]] = []):
+    def __init__(self, n: int = 0, m: int = 0, list_triple: List[Tuple[int, ...]] = None):
         self.n: int = n
         self.m: int = m
-        self.list_triple: List[Tuple[int, ...]] = list_triple
+        if list_triple is None:
+            self.list_triple = []
+        else:
+            self.list_triple: List[Tuple[int, ...]] = list_triple
 
-def parse(filename: str) -> Content:
-    """Parse the content of a file representing a graph
+
+def parse(path: str) -> Content:
+    """
+    Parse the content of a file representing a graph
 
     Parameters
     ----------
-    filename : str
-        The file name containing the data
+    path : str
+        the relative or absolute path to the input source file
 
     Returns
     -------
@@ -41,11 +47,11 @@ def parse(filename: str) -> Content:
         a object of type Content representing the content of the parsed file
     """
     content = Content()
-    file: TextIO = open("dataset/" + filename, "r")
+    file: TextIO = open(path, "r")
     first_line: str = file.readline()
     # The fist line contains the "n" and "m" value, so it has to be splitted
-    content.n: int = int(first_line.split()[0])
-    content.m: int = int(first_line.split()[1])
+    content.n = int(first_line.split()[0])
+    content.m = int(first_line.split()[1])
 
     # Continuing to analyze the file
     for line in file.readlines():
@@ -54,6 +60,6 @@ def parse(filename: str) -> Content:
         _triple: Tuple[int, ...] = int(data[0]), int(data[1]), int(data[2])
         # Adding the triple at the list inside the Content object
         content.list_triple.append(_triple)
-    
+
     file.close()
     return content
