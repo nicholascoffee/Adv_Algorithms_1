7d480e0118aff535cb72f41b5192d76b79616140
Massimiliano Baldo <bmaxium01@gmail.com>
2022-04-03 14:30:37 +0200

    Create module that implements MST with Kruskal and Union-Find

---
+++ 01-MST/kruskal_union_find.py
@@ -0,0 +1,15 @@
+from typing import List, Set
+from graph import Edge, Graph
+from union_find import UnionFindSet
+
+def kruskalUnfionFind(graph: Graph) -> Set[Edge]:
+    A: Set[Edge] = Set()
+    UF: UnionFindSet = UnionFindSet()
+    for node in graph.get_all_nodes().keys():
+        UF.make(node)
+    edges: List[Edge] = graph.get_all_edges().sort(key=lambda e: e.weight)
+    for edge in edges:
+        if (UF.find(edge.a) != UF.find(edge.b)):
+            A.add(edge)
+            UF.union(edge.a, edge.b)
+    return A
