3fb06598f372e34c22ecfc510295700ffb5502ea
Massimiliano Baldo <bmaxium01@gmail.com>
2022-04-03 14:50:59 +0200

    Change from Set to List for collect edges

--- 01-MST/kruskal_union_find.py
+++ 01-MST/kruskal_union_find.py
@@ -2,14 +2,15 @@ from typing import List, Set
 from graph import Edge, Graph
 from union_find import UnionFindSet
 
-def kruskalUnfionFind(graph: Graph) -> Set[Edge]:
-    A: Set[Edge] = set()
+def kruskalUnfionFind(graph: Graph) -> List[Edge]:
+    A: List[Edge] = []
     UF: UnionFindSet = UnionFindSet()
     for node in graph.get_all_nodes().keys():
         UF.make(node)
-    edges: List[Edge] = graph.get_all_edges().sort(key=lambda e: e.weight)
+    edges: List[Edge] = graph.get_all_edges()
+    edges.sort(key=lambda e: e.weight)
     for edge in edges:
         if (UF.find(edge.a) != UF.find(edge.b)):
-            A.add(edge)
+            A.append(edge)
             UF.union(edge.a, edge.b)
     return A
