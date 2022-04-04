134c71adf1268f99c3a933862881a2c6cef46cbf
Enrico <enrico.galdeman@live.com>
2022-04-04 14:09:27 +0200

    first commit
    primo commit di prova

---
+++ 01-MST/naive_kruskal.py
@@ -0,0 +1,68 @@
+from graph import Graph
+
+def sorted_edges(graph: Graph) -> list:
+    edges = Graph.get_all_edges
+    edges.sort()
+    return edges
+
+# A recursive function that uses visited[] and parent to detect cycle in subgraph reachable from vertex v
+
+def isCyclicUtil(self,v,visited,parent):
+ 
+    # Mark the current node as visited
+    visited[v]= True
+ 
+    # Recur for all the vertices adjacent to this vertex
+    for i in self.graph[v]: 
+        # If the node is not visited then recurse on it
+        if  visited[i]==False :
+            
+            if(self.isCyclicUtil(i,visited,v)):
+            
+                return True
+        
+        # If an adjacent vertex is visited and not parent of current vertex, then there is a cycle
+        elif  parent!=i:
+            
+            return True     
+
+    return False
+# Returns true if the graph contains a cycle, else false.
+
+def isCyclic(self):
+       
+    # Mark all the vertices as not visited
+    visited = [False] * (self.V)
+     
+    # Call the recursive helper function to detect cycle in different DFS trees
+    
+    for i in range(self.V):
+           
+        # Don't recur for u if it is already visited
+        
+        if visited[i] ==False:
+
+            if(self.isCyclicUtil(i,visited,-1)) == True:
+            
+            return True
+         
+    return False
+
+
+def naive_kruskal(graph: Graph) -> Graph:
+
+    #empty graph to return with the solution
+    
+    A_Graph = new graph
+    nodes = Graph.get_all_nodes
+    
+    #sorting the edges in crescent order
+
+    edges = sorted_edges(Graph)
+    
+    #check for all the edges sorted the nodes
+    for v in nodes.values
+
+    #add in the new graph if the graph created is still acyclic, else continue with the next ndoes
+
+    pass
