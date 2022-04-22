d404430f3fdf2dc0c6977c59f1f4c0d874dca2a5
Massimiliano Baldo <bmaxium01@gmail.com>
2022-04-22 22:54:45 +0200

    Change return from list of edges to graph

--- 01-MST/algorithms/kruskal_union_find.py
+++ 01-MST/algorithms/kruskal_union_find.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Tuple
+from typing import List
 from datastructure.graph import Edge, Graph
 from datastructure.union_find import UnionFindSet
 
@@ -19,16 +19,34 @@ def sort_edges(graph: Graph) -> List[Edge]:
     graph.static_edges.sort()
     return graph.static_edges
 
+def kruskal_union_find(graph: Graph) -> Graph:
+    """
+    Returns the Minimum Spanning Tree of the given graph using Kruskal with Union Find (based on union-by-size)
 
-def kruskal_union_find(graph: Graph):
-    result = []
+    Parameters
+    ----------
+    graph : Graph
+        the graph on which calculate the Minimum Spanning Tree
+
+    Returns
+    -------
+    Graph :
+        Minimum Spanning Tree graph based on the input graph
+    """
+    mst: Graph = Graph()
     union_find: UnionFindSet = UnionFindSet()
+
+    # Create all the initial sets of union-find
     for node in graph.get_all_nodes():
         union_find.make(node)
+    
+    # Sort the edges of the graph
     edges: List[Edge] = sort_edges(graph)
 
+    # Check if an edge is not inside anthor set, then make the union of the sets
+    # and add that edge to the MST
     for edge in edges:
         if union_find.find(edge.a) != union_find.find(edge.b):
-            result.append(edge)
+            mst.add_edge(edge.a, edge.b, edge.weight)
             union_find.union(edge.a, edge.b)
-    return result
+    return mst
