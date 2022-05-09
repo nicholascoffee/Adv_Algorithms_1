019e19bf042d6b988c53ac88b1cb4ce0ce6e7a76
Nicholas Miazzo <nicholas.miazzo1998@gmail.com>
2022-05-09 13:15:22 +0200

    Improve graph

--- 02-TSP/graph.py
+++ 02-TSP/graph.py
@@ -1,16 +1,16 @@
-from collections import defaultdict
 from dataclasses import dataclass
-from typing import DefaultDict, Dict, List
+from typing import Dict, List, Generator
 import numpy as np
+
 
 @dataclass
 class Node:
     """
-    Dataclass for rappresent a node inside a graph.
+    Dataclass for represent a node inside a graph.
 
     Attributes
     ----------
-    id: int
+    i: int
         the identifier for the node. Must be unique
     x: int
         the latitude coordinate of the node
@@ -22,26 +22,25 @@ class Node:
     x: int
     y: int
 
-    def __init__(self, id: int, x: int, y: int) -> None:
-        self.id = id
+    def __init__(self, i: int, x: int, y: int) -> None:
+        self.id = i
         self.x = x
         self.y = y
+
 
 @dataclass
 class Graph:
     """
-    Dataclass for rappresent a graph of nodes.
+    Dataclass for represent a graph of nodes.
 
     Attributes
     ----------
     name: str
-        the name of the graph for more readdable reference for optimal solution
+        the name of the graph for more readable reference for optimal solution
     n: int
         the total number of nodes in the graph
     weight_type: str
         the type of representation of the nodes. Useful to know how to calculate distance between nodes
-    adjacency_list: DefaultDict[int, List[Node]]
-        a dict to store all the adjancent nodes respect to a specific node
     nodes: Dict[int, Node]
         a list to store all the nodes present in the graph
     weights: np.ndarray
@@ -50,9 +49,6 @@ class Graph:
     name: str
     n: int
     weight_type: str
-    # Map all the neighbors of the nodes in the graph
-    # TODO: Is it necessary? Can we use only the nodes attribute?
-    adjacency_list: DefaultDict[int, List[Node]]
     # Store all the nodes to access more quickly
     nodes: Dict[int, Node]
     # Store the weights of alle the edges in a matrix
@@ -62,43 +58,32 @@ class Graph:
         self.name = name
         self.n = n
         self.weight_type = weight_type
-        self.adjacency_list = defaultdict(list)
         self.nodes = {}
         self.weights = np.zeros((n, n), dtype=np.int16)
 
     def _get_distance(self, first: Node, second: Node) -> int:
-        # TODO: implement the module for calculate distance based on weigth_type
+        # TODO: implement the module for calculate distance based on weigh_type
         return 1
 
-    def add_node(self, id: int, x: int, y: int) -> None:
+    def add_node(self, i: int, x: int, y: int) -> None:
         """
-        Insert a new node in the graph with sepcific attributes
+        Insert a new node in the graph with specific attributes
 
         Parameters
         ----------
-        id: int
+        i: int
             is the identifier for the new node
         x: int
             is the latitude coordinate for the new node
         y: int
             is the longitude coordinate for the new node
         """
-        new_node: Node = Node(id, x, y)
-        nodes: Dict[int, Node] = self.nodes
-        adj: DefaultDict[int, List[Node]] = self.adjacency_list
-
-        # Add the new node to all adjacency lists of the other nodes
-        for list_node in adj.values():
-            list_node.append(new_node)
-
-        # Add all the other nodes to the adjacency list of the new node
-        adj[id] = list(nodes.values())
         # Insert new node in the list of all nodes in the graph
-        nodes[id] = new_node
+        self.nodes[i] = Node(i, x, y)
 
     def get_nodes(self) -> List[Node]:
         """
-        Retrive all the nodes present in the graph
+        Retrieve all the nodes present in the graph
 
         Returns
         -------
@@ -107,39 +92,28 @@ class Graph:
         """
         return list(self.nodes.values())
 
-    def get_adj_nodes(self, id: int) -> List[Node]:
-        """
-        Retrive all the adajcent nodes of a node
-
-        Parameters
-        ----------
-        id: int
-            the identifier of the node of which the neighboors are searched
-
-        Returns
-        -------
-        List[Node]
-            a lis of nodes that are in the graph
-        """
-        return self.adjacency_list[id]
+    def adj_nodes(self, i: int):
+        for node in self.get_nodes():
+            if node.id != i:
+                yield node
 
     def calculate_weights(self) -> None:
         """
-        Calcute all the distance in the nodes inside the graph.
-        MUST BE USE ONLY WHEN THE GRAPH IS COMPLETE.S
+        Calculate all the distance in the nodes inside the graph.
+        MUST BE USE ONLY WHEN THE GRAPH IS COMPLETE.
         """
-        # Considering evrey node in the graph
+        # Considering every node in the graph
         for node in self.nodes.values():
             # and considering all the other nodes
-            for adj_node in self.get_adj_nodes(node.id):
+            for adj_node in self.adj_nodes(node.id):
                 # Calculate the distance between the nodes and store the result in the matrix
                 # Indexes start from 0, but nodes start from 1
                 self.weights[node.id - 1, adj_node.id - 1] = self._get_distance(node, adj_node)
 
     def print(self) -> None:
         """
-        Print all the infromations of the graph
+        Print all the information of the graph
         """
         print(self.name, self.n, self.weight_type)
-        for node, list_node in self.adjacency_list.items():
-            print(node, ":", list_node)
+        for node in self.nodes:
+            print(node, ":", list(self.adj_nodes(node)))
