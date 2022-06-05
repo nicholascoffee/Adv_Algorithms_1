a4e4c9915741bc4dde8d51c8b18d95c45b472928
Massimiliano Baldo <bmaxium01@gmail.com>
2022-06-05 18:36:34 +0200

    Implement the random select and edge select

--- 03-MCP/algorithms/karger_stein.py
+++ 03-MCP/algorithms/karger_stein.py
@@ -61,3 +61,48 @@ def __binary_search(values: List[Node], r: int) -> Node:
         return __binary_search(values[:pivot], r)
     else: # r > values[pivot]
         return (pivot + __binary_search(values[pivot:], r))
+
+def __random_select(cumulative_dict: Dict[Node, int]) -> Node:
+    """
+    Given a cumulative dictionary of weights, the function generates a random integer
+    r such that 0 <= r < cumulative_dict[last-value] and use it to find a good node for
+    the cut.
+
+    Parameters
+    ----------
+    cumulative_dict: Dict[Node, int]
+        is a dictionary of cumulative weights from which start finding the node 
+
+    Returns
+    -------
+    Node
+        the id of the node for the cut in the graph
+    """
+    # Take a random value from 0 to the last value of the cumulative dictionary
+    last_value: int = list(cumulative_dict.values())[-1]
+    r: Node = randrange(last_value)
+    u: Node = __binary_search(list(cumulative_dict.values()), r)
+    return u
+
+def __edge_select(g: Graph) -> Tuple[Node, Node]:
+    """
+    Given a graph, the function returns a pair of nodes which represents a good minimun cut.
+
+    Parameters
+    ----------
+    g: Graph
+        is the graph where to find the nodes for the cut
+
+    Returns
+    -------
+    Tuple[Node, Node]
+        is the pair of nodes (i.e. the edge) good for the minuimun cut
+    """
+    # Build cumulative weights array on weighted degree
+    c1: Dict[Node, int] = __cumulative_weighted_array(g.weighted_degree)
+    u: Node = __random_select(c1)
+    c2: Dict[Node, int] = __cumulative_weighted_array2(g.weighted_matrix[u])
+    v: Node = __random_select(c2)
+    # TODO: Assertion for avoid self-loop or non existing edges
+    assert (v != u) and (g.get_weight(u, v) != 0), "Self-Loop or Not Existing Edge returned"
+    return u, v
