b671882d10c237cbea26f20daa2abc263ce1831e
Massimiliano Baldo <bmaxium01@gmail.com>
2022-06-17 15:38:25 +0200

    Add comments in the graph.py file

--- 03-MCP/graph.py
+++ 03-MCP/graph.py
@@ -1,39 +1,37 @@
 from collections import defaultdict
 from dataclasses import dataclass, field
-from typing import DefaultDict, Dict, List, Optional, Tuple, Set
+from typing import DefaultDict, Dict, List, Tuple
 import numpy as np
 import parser
 
+# Type Aliases
 Node = int
 Edges = Dict[Tuple[Node, Node], int]
-
 
 @dataclass
 class Graph:
     """
-    A class for represent an undirected simple graph using adjacency lists
+    A class for represent an undirected simple graph.
 
-    Attributes
-    __________
-    adjacency_list : Dict[int, List[AdjacencyNode]]
-
-    edges : Edges
-
+    Attributes:
+    -----------
     n : int
         number of nodes in the graph
     m : int
         number of edges in the graph
-
+    nodes: List[Node]
+        list containing all id of nodes inside the graph
+    weighted_matrix: np.ndarray
+        a matrix for store weights of edges in the graph
+    weighted_degree: DefaultDict[Node, int]
+        a dictionary to store the weighted degree of nodes in the graph
     """
     n: int = field(default=0)
     m: int = field(default=0)
-    # TODO: we need edges?
-    # edges: Edges = field(default_factory=dict)
+    nodes: List[Node] = field(default_factory=lambda: list())
     # Datastructure for the algorithms of Minimum Cut
     weighted_matrix: np.ndarray = field(init=False)
     weighted_degree: DefaultDict[Node, int] = field(default_factory=lambda: defaultdict(int))
-
-    nodes: List[Node] = field(default_factory=lambda: list())
 
     def __post_init__(self):
         # Defer the initialization of the matrix since depends on an another field of the class
@@ -69,12 +67,46 @@ class Graph:
         degree[b] += weight
 
     def get_nodes(self) -> List[Node]:
+        """
+        Returns the nodes inside the graph
+
+        Returns:
+        --------
+        List[Node]
+            a list of nodes
+        """
         return self.nodes
 
     def get_weight(self, a: int, b: int) -> int:
+        """
+        Given two nodes inside the graph, the function returns the weight
+        for travelling from node a to node b if the edge exists, otherwise 
+        returns 0.
+
+        Parameters
+        ----------
+        a: int
+            is the first node in the edge
+        b: int
+            is the second node in the edge
+        
+        Returns:
+        --------
+        int
+            the cost for go from node a to node b
+        """
         return self.weighted_matrix[a][b]
 
     def adj_nodes(self, node_id: int):
+        """
+        Given the id of a node, the function returns the adjacent nodes and their
+        weights inside an iterator 
+
+        Parameters
+        ----------
+        node_id: int
+            is the is of the node 
+        """
         for node in self.get_nodes():
             weight: int = self.weighted_matrix[node_id][node]
             if node != node_id and weight != 0:
